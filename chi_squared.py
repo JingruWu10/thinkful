3dd8a50733ec0ebc657e186f916d6ab4bce0d036
JingruWu10 <jingruwu12@gmail.com>
2016-04-20 11:44:57 -0700

    Update chi_squared.py

--- chi_squared.py
+++ chi_squared.py
@@ -1,14 +1,16 @@
 from scipy import stats
 import collections
+import pandas as pd
 
 #Loads data
 loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
 
 #cleans data
 loansData.dropna(inplace=True)
-
+freq=collections.Counter(loansData['Open.CREDIT.Lines'])
 #performs chi squared
 chi, p = stats.chisquare(freq.values())
 
 #prints
 print chi, p
+
