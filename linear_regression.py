e4023cdc60a3561b81b86565aba6b55679a7ca74
JingruWu10 <jingruwu12@gmail.com>
2016-04-20 12:13:02 -0700

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,70 +1,85 @@
+import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
-import numpy as np 
-import statsmodels.api as sm 
+import statsmodels.api as sm
 
-# get the data
 loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
 
-# remove '%' sign from interest rate data
-rmPercentSign = lambda x: float(x[:x.find('%')])
-loansData['Interest.Rate'] = map(rmPercentSign, loansData['Interest.Rate'])
+## cleaning the file
+loansData['Interest.Rate'] = loansData['Interest.Rate'].str.rstrip('%').astype(float).round(2) / 100.0
 
-# remove word 'month' from loan length data
-rmWordMonth = lambda x: float(x[:x.find(' ')])
-loansData['Loan.Length'] = map(rmWordMonth, loansData['Loan.Length'])
+loanlength = loansData['Loan.Length'].str.strip('months')#.astype(int)  --> loanlength not used below
 
-# select first element from range in FICO Range and turn to int
-cleanFICO = loansData['FICO.Range'].map(lambda x: x.split('-'))
-cleanFICO = cleanFICO.map(lambda x: [int(n) for n in x])
-loansData['FICO.Score'] = cleanFICO.map(lambda x: x[0])
+loansData['FICO.Score'] = loansData['FICO.Range'].str.split('-', expand=True)[0].astype(int)
 
-# plot FICO Score
-plt.figure()
-p = loansData['FICO.Score'].hist()
+#add interest rate less than column and populate
+## we only care about interest rates less than 12%
+loansData['IR_TF'] = loansData['Interest.Rate'] < 0.12
+
+#create intercept column
+loansData['Intercept'] = 1.0
+
+# create list of ind var col names
+ind_vars = ['FICO.Score', 'Amount.Requested', 'Intercept'] 
+
+#define logistic regression
+logit = sm.Logit(loansData['IR_TF'], loansData[ind_vars])
+
+#fit the model
+result = logit.fit()
+
+#get fitted coef
+coeff = result.params
+
+#print coeff
+print result.summary() #result has more information
+print coeff
+
+def logistic_function(fico_score, loan_amount, coefficients):
+    b, a1, a2 = coefficients   
+    x = b + a1*fico_score + a2*loan_amount
+    p = 1./(1.+np.exp(-x))
+    return p
+
+p1 = logistic_function(720, 10000, coeff)
+print p1
+print p1>0.70
+
+def prep(fico_score, loan_amount, coefficients):
+    return logistic_function(fico_score, loan_amount, coefficients) > 0.70
+print prep(400, 100000, coeff)
+
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+colors = ["r" if bool(ib12) else "b" for ib12 in loansData['IR_TF'] ]
+ax.scatter(loansData['FICO.Score'], loansData['Amount.Requested'],  loansData['IR_TF'], 
+           c=colors)
+ax.set_xlabel('FICO SCORE')
+ax.set_ylabel('Amount Requested')
+ax.set_zlabel('Interest Below 0.12?')
 plt.show()
-# FICO scores are skewed to the right (similar to open credit lines)
 
-# create scatter plot matrix
-plt.figure()
-a = pd.scatter_matrix(loansData, alpha = 0.05, figsize=(10,10), diagonal='hist')
+#plot your data and surface#
+xmin = loansData['FICO.Score'].min()
+xmax = loansData['FICO.Score'].max()
+ymin = loansData['Amount.Requested'].min()
+ymax = loansData['Amount.Requested'].max()
+N = 10
+x = np.linspace(xmin, xmax, 20)
+y = np.linspace(ymin, ymax, 20)
+X, Y = np.meshgrid(x,y)
+Z = logistic_function(X,Y,coeff)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+colors = ["r" if bool(ib12) else "b" for ib12 in loansData['IR_TF'] ]
+ax.scatter(loansData['FICO.Score'], loansData['Amount.Requested'],  loansData['IR_TF'], 
+           c=colors)
+preds = result.predict(loansData[ind_vars])
+ax.scatter(loansData['FICO.Score'], loansData['Amount.Requested'],  preds, c="y")
+ax.plot_wireframe(X,Y,Z,alpha=0.5)
+ax.set_xlabel('FICO SCORE')
+ax.set_ylabel('Amount Requested')
+ax.set_zlabel('Interest Below 0.12?')
 plt.show()
-# most interesting relationship is between income and FICO scores
-# Thinkful points out that loan amount probably impacts interest rate
 
-# build lineal model to predict interest rate
-# independent vars = FICO score and loan amount
-intrate = loansData['Interest.Rate']
-loanamt = loansData['Amount.Requested']
-fico = loansData['FICO.Score']
-
-# reshape data from df to matrix
-y = np.matrix(intrate).transpose()
-x1 = np.matrix(fico).transpose()
-x2 = np.matrix(loanamt).transpose()
-
-# combine x columns to create input matrix
-x = np.column_stack([x1,x2])
-
-# create model
-X = sm.add_constant(x)
-model = sm.OLS(y,X)
-f = model.fit()
-f.summary()
-
-print 'Coefficients: ', f.params[1:]
-print 'Intercept: ', f.params[0]
-print 'P-Values: ', f.pvalues
-print 'R-Squared: ', f.rsquared
-print "Final model: 'Interest Rate = " + str(round(f.params[0])) + " + " + str(f.params[1]) + "*fico" + ' + ' + str(f.params[2]) + "*loanamt"
-
-# the p values are less than 0.05 so we can reject the null and assume all relationships are significant
-# the r2 = .65 so our model explains roughly 65% of variation in interest rates
-# TODO: consider re-hashing with feature selection package in R (stepwise selection)
-
-def linear_function(FICO, Amount):
-	print "FICO = " + str(FICO)
-	print "Amount = " + str(Amount)
-	interest_rate = (f.params[0] + f.params[1]*FICO + f.params[2]*Amount)
-	print "Expected interest rate = " + str(interest_rate)
-print linear_function(600,10000)
